01d0941f8187a250aa499cde59a9c18db27179c4
LoganArnett <Loganarn5@gmail.com>
2015-04-19 15:53:04 -0400

    have animations firing on scroll and basic layout

--- src/app/plain.js
+++ src/app/plain.js
@@ -1,12 +1,14 @@
-$(window).scroll(function() {    
+
+    $( window ).scroll(function() {    
     // get the amount the window has scrolled
     var scroll = $(window).scrollTop();
     // add the 'active' class to the correct li based on the scroll amount
-    if (scroll <= 500) {
-        $("#html").addClass("animated fadeInLeft");
+    if (scroll >= 200) {
+        $("#html").removeClass("hidden").addClass("animated fadeInUp");
+        $("#angular").removeClass("hidden").addClass("animated fadeInUp delay-half");
+        $("#ionic").removeClass("hidden").addClass("animated fadeInUp delay-one");
+        $("#what-we-do").removeClass("hidden").addClass("animated fadeInUp");
     }
-    else if (scroll <= 1000) {
-        $("#angular").addClass("animated bounceInDown");
-        $("#ionic").addClass("animated fadeInRight");
-    }
-};
+    
+});
+   
